6476298919c04989fa1488a533e65483e13cccd1
foobar warren <foobar.warren@gmail.com>
2019-12-31 17:20:10 -0800

    test number 2

--- pyCode/images/displayMandrill.py
+++ pyCode/images/displayMandrill.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt  
 
-# test of github
+# test of github 2
 img = cv2.imread('/home/system/Desktop/ws/myPy/data/baboon.jpg',cv2.IMREAD_COLOR)
 cv2.imshow('img',img)
 cv2.waitKey(0)
